f11b1a8ecea0a7dc1ba48917f459e1d97ece12bc
DarellYuhu <darell.mona242@gmail.com>
2025-07-17 13:16:42 +0700

    chore: fix build?

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -4,6 +4,7 @@ import { getTopPick } from "@/api/top-picks";
 import { Highlight } from "@/components/highlight";
 import { TopPicks } from "@/components/top-picks";
 
+export const dynamic = "force-dynamic";
 export default async function HomePage() {
   const highlights = await getHighligts();
   const populars = await getPopularArticles();
